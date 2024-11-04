67859194ca9086b92d8b099738d667c400964c9c
Marjeremy <gaoat856@gmail.com>
2024-11-04 18:55:04 +0800

    classSubject.js commit

---
+++ classSubject.js
@@ -0,0 +1,7 @@
+let subjectTitle = "Mathematics";
+let classSchedule = "10:00 AM - 11:30 AM, Monday and Wednesday";
+let classroom = "Comlab";
+let classInstructor = "RBTN";
+let studentName = "Mark Jeremy Gaoat"
+
+console.log(studentName + " is currently enrolled in " + subjectTitle + " with a class schedule of " + classSchedule + " at room " + classroom + ". The instructor for the subject is " + classInstructor);
